bb7bbff508bffc5fc72532f1722c0560aead29ea
krim <zaurkuzeev456@gmail.com>
2025-11-08 20:02:21 +0300

    add controller to currency-analytics-service, Dockerfile, moved api-key from application.yml to .env and add .gitignore + add module to the dockar-compose.yaml

---
+++ currency-analytics-service/src/main/java/com/example/currencyanalyticsservice/controller/CurrencyAnalyticsController.java
@@ -0,0 +1,53 @@
+package com.example.currencyanalyticsservice.controller;
+
+
+import com.example.currencyanalyticsservice.service.CurrencyAnalyticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/CurrencyAnalytics")
+public class CurrencyAnalyticsController {
+
+    private final CurrencyAnalyticsService currencyAnalyticsService;
+
+    @PostMapping("/averageBetweenDate")
+    public Double averageBetweenDate(@RequestParam String targetCode,
+                                     @RequestParam LocalDateTime beginDate,
+                                     @RequestParam LocalDateTime endDate) {
+
+        return currencyAnalyticsService.averageBetweenDate(targetCode, beginDate, endDate);
+
+    }
+
+    @PostMapping("/averageAllTime")
+    public Double averageAllTime(@RequestParam String targetCode) {
+        return currencyAnalyticsService.averageAllTime(targetCode);
+    }
+
+    @PostMapping("/maxMinValue")
+    public Double maxMinValue(@RequestParam String targetCode) {
+        return currencyAnalyticsService.maxMinValue(targetCode);
+    }
+
+    @PostMapping("/volatility")
+    public Double currencyVolatility(@RequestParam String targetCode) {
+        return currencyAnalyticsService.currencyVolatility(targetCode);
+    }
+
+    @PostMapping("/ratioPercentage")
+    public String ratioPercentage(@RequestParam String targetCode,
+                                  @RequestParam LocalDateTime beginTime,
+                                  @RequestParam LocalDateTime endTime) {
+
+        return currencyAnalyticsService.ratioPercentage(targetCode, beginTime, endTime);
+    }
+
+
+}
